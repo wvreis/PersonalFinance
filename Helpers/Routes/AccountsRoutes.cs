e9617221e3bc4d298e8aaa759f5767ce482ebb5e
Wesley Valentim <wv.reis@gmail.com>
2023-07-16 23:48:40 -0300

    Continuação de implementação de cadastro de contas.

--- Helpers/Routes/AccountsRoutes.cs
+++ Helpers/Routes/AccountsRoutes.cs
@@ -2,5 +2,8 @@
 public static class AccountsRoutes {
     public const string Index = "/accounts";
     public const string Addition = $"{Index}/addition";
-    public const string Edition = $"{Index}/edition";
+    public const string Edition = $"{Index}/edition/{{id:int}}";
+
+    public static string GoToEdition(int id) =>
+        $"{Index}/edition/{id}";
 }
